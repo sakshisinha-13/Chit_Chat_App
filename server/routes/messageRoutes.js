b3fb7b931a36c18db644abad70795871c05c777e
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-05-25 16:02:16 +0530

    Delete server directory

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const { addMessage, getMessages } = require("../controllers/messagesController");
-
-const router = require("express").Router();
-
-router.post("/addmsg", addMessage);
-router.post("/getmsg", getMessages);
-
-module.exports = router;
