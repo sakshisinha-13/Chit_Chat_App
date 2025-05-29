1c2573301fe2d4a7257d72fafb77e0ab57736f55
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-05-29 14:07:57 +0530

    All files added.

--- ChatApp-main/public/src/components/Messages.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-export default function Messages(){
-    return <Container>Msg</Container>
-} 
-
-const Container=styled.div`
-    height: 80%;
-`;
